7ca98fb98d78c6fb0f54ca92bf77ec6595446b5a
Rodrigo Costa <rodrigo.costa@gmail.com>
2024-05-08 01:15:43 -0300

    Prettier and Husky

--- keeper-app/src/App.jsx
+++ keeper-app/src/App.jsx
@@ -1,15 +1,16 @@
-import './App.css'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import Note from './components/Note'
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Note from "./components/Note";
 
 function App() {
-
-  return (<>
+  return (
+    <>
       <Header />
       <Note />
       <Footer />
-    </>)
+    </>
+  );
 }
 
-export default App
+export default App;
